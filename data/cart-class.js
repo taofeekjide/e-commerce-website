8f1c8eff1bf4429dbb6af3bcd4b266e825d216fa
Taofeek Jide-Idowu <149229855+taofeekjide@users.noreply.github.com>
2025-03-20 14:48:46 +0100

    Update cart-class.js

--- data/cart-class.js
+++ data/cart-class.js
@@ -10,23 +10,6 @@ class Cart {
 
   #loadFromStorage() {
     this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey)) || [];
-
-    /*
-    if (!this.cartItems) {
-      this.cartItems = [
-        {
-          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-          quantity: 2,
-          deliveryOptionId: "1",
-        },
-        {
-          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-          quantity: 1,
-          deliveryOptionId: "2",
-        },
-      ];
-    }
-    */
   }
 
   saveToLocalstorage() {
